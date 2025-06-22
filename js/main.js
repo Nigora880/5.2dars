9fdd8a8542da0914abae7d6648ed269728504233
Nigora880 <nx165880@gmail.com>
2025-06-23 00:37:07 +0300

    commit

--- js/main.js
+++ js/main.js
@@ -6,14 +6,26 @@
 
 // users part
 let elUsersList = document.querySelector(".users-list")
-const getUsers = (url) => fetch(url).then(res => res.json()).then(data => renderUsers(data, elUsersList))
+let elPostsList = document.querySelector(".posts-list")
+let elCommentsList = document.querySelector(".comments-list")
+
+const getUsers = (url) => {
+    elUsersList.innerHTML = `
+    <li>
+       <img class="absolute scale-[1] top-0 bottom-0 left-0 right-0 m-auto" src="./img/loading.png" alt="loading " width="100" height="100">
+    </li>
+`
+  fetch(url).then(res => res.json()).then(data => renderUsers(data, elUsersList))
  
+}
+    
+
 function renderUsers(usersList,saveList){
     saveList.innerHTML = null
     usersList.forEach(item => {
         let elItem = document.createElement("li")
+         elItem.className = "flex bg-slate-200 cursor-pointer rounded-md p-3 items-center justify-between"
         elItem.innerHTML = `
-         <li class="flex bg-slate-300 rounded-md p-3 items-center justify-between ">    
      <div class="flex items-center gap-[20px]">
         <img class="rounded-full w-[100px] h-[100px] rounded-full" src="./img/phone-img-removebg-preview.png" alt="user img" width="100" height="100">
      <div>
@@ -21,12 +33,23 @@ function renderUsers(usersList,saveList){
         <p class="text-[18px]" >${item.email}</p>
     </div>
     </div>
-    <button>
+    <button class="">
         <img src="./img/dots.svg" alt="dots" width="30" height="30">
     </button>
-     </li>
         `
         saveList.appendChild(elItem)
+
+        elItem.addEventListener("click", () =>{
+             getPostById(item.id)
+             
+            })
     })
 }
 getUsers("https://jsonplaceholder.typicode.com/users")
+
+// posts
+function getPostById(id){
+    fetch (`https://jsonplaceholder.typicode.com/posts?userId=${id}`).then(res => res.json()).then(data => {}).catch(err => {
+
+    }).finally()
+}
